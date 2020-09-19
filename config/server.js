babc402cbbc079affb4cb81dda476756d7a27c60
Marco Bassaletti <bassaletti@gmail.com>
2020-09-19 13:13:35 -0300

    Initial strapi installation.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'afa6423f51d8686883ac38146a617bc7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ecb9cd6a-4e28-482b-82a2-081f2edf1d28'
+};
