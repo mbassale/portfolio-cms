bc540e3a554ec97d9e4d9e240ce03029908ccab0
Marco Bassaletti <bassaletti@gmail.com>
2020-10-04 16:27:01 -0300

    PM2 config fix.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,12 @@ module.exports = {
     {
       name: 'strapi',
       cwd: '/var/www/cms.mbassale.cl',
-      script: 'yarn',
+      script: 'npm',
       args: 'start',
       env: {
         NODE_ENV: 'production',
+        HOST: '0.0.0.0',
+        PORT: 1337,
         DATABASE_FILENAME: '.tmp/data.db',
       },
     },
